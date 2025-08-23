45aa2375118a088241caab3d987993dfa77ed5de
Ondrej Holecek <ondrej@vivaldi.com>
2025-08-23 17:33:33 +0200

    dev

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,11 @@
+#![allow(dead_code)]
+#![allow(unused_variables)]
+#![allow(unused_imports)]
 mod c1;
 mod chat;
 mod ask_prompt_component;
 
-use gloo::net::http::Request;
+use gloo::net::http::{Request, Response};
 use yew::prelude::*;
 use yew_router::prelude::*;
 use yew_router::hooks::use_navigator;
@@ -16,7 +19,10 @@ use wasm_bindgen::JsValue;
 use anyhow::{Context, Result};
 
 
-struct SoftState {}
+struct SoftState {
+    n: UseStateHandle<i32>,
+    error_message: UseStateHandle<String>,
+}
 
 
 #[derive(Clone, Routable, PartialEq)]
@@ -25,6 +31,8 @@ enum Route {
     Home,
     #[at("/game")]
     Game,
+    #[at("/error")]
+    Error,
     #[not_found]
     #[at("/404")]
     NotFound,
@@ -52,6 +60,7 @@ fn switch(routes: Route) -> Html {
     match routes {
         Route::Home => html! { <Home /> },
         Route::Game => html! { <Game /> },
+        Route::Error => html! { <Error /> },
         Route::NotFound => html! { <h1>{ "404" }</h1> },
     }
 }
@@ -59,17 +68,16 @@ fn switch(routes: Route) -> Html {
 #[function_component]
 fn Home() -> Html {
     use_state(|| {});
-    //use_context::<SoftState>();
     let navigator = use_navigator().unwrap();
     let onclick = Callback::from(move |_| wasm_bindgen_futures::spawn_local({
         let navigator = navigator.clone();
         async move {
-            if let Ok(token) = fetch_text("/api/token").await {
+            if let Ok(token) = fetch_text("/api/new_game").await {
                 info!("res: {:?}", token);
                 LocalStorage::set("token", &token).unwrap();
                 navigator.push(&Route::Game);
             } else {
-                navigator.push(&Route::NotFound);
+                navigator.push(&Route::Error);
             }
         }
     }));
@@ -84,15 +92,26 @@ fn Home() -> Html {
 
 async fn fetch_text(path: &str) -> Result<String> {
     let res = Request::get(path).send().await?;
+    if res.status() != 200 {
+        return Err(anyhow::anyhow!("status[{}]: {}", path, res.status()));
+    }
     let text = res.text().await?;
-    info!("res: {:?}", text);
+    info!("res: {:?} {:?}", text, res.status());
     Ok(text)
+}
+
+async fn send_question(token: &str, text: &str) -> Result<()> {
+    info!("{}: asking: {}", token, text);
+    Err(anyhow::anyhow!("ask"))
 }
 
 
 #[function_component]
 fn Game() -> Html {
     let navigator = use_navigator().unwrap();
+    let debug = use_state(|| { String::new() });
+    let question_in_air = use_state(|| { false });
+
     let token: String = match LocalStorage::get("token").ok() {
         Some(token) => token,
         None => {
@@ -100,10 +119,29 @@ fn Game() -> Html {
             return html! {};
         }
     };
+
+    {
+        let debug = debug.clone();
+        use_effect_with(token.clone(), move |token: &String| {
+            let token = token.clone();
+            let debug = debug.clone();
+            wasm_bindgen_futures::spawn_local(async move {
+                match fetch_text(&format!("/api/game/{token}")).await {
+                    Ok(res) => debug.set(res),
+                    Err(e) => log::error!("fetch /api/game failed: {e:?}"),
+                }
+            });
+            || ()
+        });
+    }
+
     let on_send = {
-        let navigator = navigator.clone();
+        let token = token.clone();
         Callback::from(move |text: String| {
-            info!("on_send: {}", text);
+            let token = token.clone();
+            wasm_bindgen_futures::spawn_local(async move {
+                send_question(&token, &text).await;
+            });
         })
     };
 
@@ -111,6 +149,9 @@ fn Game() -> Html {
         <>
         <h1>{ token }</h1>
         <AskPrompt prompt={"Make your guess..."} on_send={on_send}/>
+        <hr/>
+        <pre>{ debug.to_string() }</pre>
+        <pre>{ question_in_air.to_string() }</pre>
         </>
     }
 }
@@ -123,6 +164,13 @@ fn App() -> Html {
         <BrowserRouter>
             <Switch<Route> render={switch} /> // <- must be child of <BrowserRouter>
         </BrowserRouter>
+    }
+}
+
+#[function_component]
+fn Error() -> Html {
+    html! {
+        <h1>{ "Error" }</h1>
     }
 }
 
